8a0ecfa1d67341bc0ad97d1888fb6da66d6a146d
kirtan1216 <kirtandobariya1216@gmail.com>
2024-05-17 00:53:21 +0530

    all commit

---
+++ c++ apna collage/prime.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b"<<endl;
+    cin>>b;
+
+    cout<<a<<endl<<b;
+}    
