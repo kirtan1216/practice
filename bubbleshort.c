7dd7e89a96d259e6bc70e430daf61e3752b91bad
kirtan1216 <kirtandobariya1216@gmail.com>
2024-05-17 20:02:28 +0530

    Reapply "hello it is demo off the makers"
    This reverts commit f1daca9e7974ec0349fee542ec5bc42d570fae10.

---
+++ bubbleshort.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+ int main(){
+    printf("hello i am chekcing somthing for the git repo so be helpfull in the process");
+    printf("hello i am chekcing somthing for the git repo so be helpfull in the process");
+   
+  
+    return 0;
+    
+
+ }
+
+
+
+
+
+
